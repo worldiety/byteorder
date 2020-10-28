de801a68f7c1dd53dc21596b881e45783e7c20ed
Torben Schinke <torben.schinke@worldiety.de>
2020-10-28 12:13:13 +0100

    added test as a "visual" comparision

--- int_test.go
+++ int_test.go
@@ -17,6 +17,7 @@
 package byteorder_test
 
 import (
+	"fmt"
 	"testing"
 
 	. "github.com/worldiety/byteorder"
@@ -24,104 +25,104 @@ import (
 
 //nolint:funlen
 func TestConst(t *testing.T) {
-	if MaxInt8 != 127 {
+	if fmt.Sprintf("%v", MaxInt8) != "127" {
 		fail(t)
 	}
 
-	if MinInt8 != -128 {
+	if fmt.Sprintf("%v", MinInt8) != "-128" {
 		fail(t)
 	}
 
-	if MaxInt16 != 32767 {
+	if fmt.Sprintf("%v", MaxInt16) != "32767" {
 		fail(t)
 	}
 
-	if MinInt16 != -32768 {
+	if fmt.Sprintf("%v", MinInt16) != "-32768" {
 		fail(t)
 	}
 
-	if MaxInt24 != 8388607 {
+	if fmt.Sprintf("%v", MaxInt24) != "8388607" {
 		fail(t)
 	}
 
-	if MinInt24 != -8388608 {
+	if fmt.Sprintf("%v", MinInt24) != "-8388608" {
 		fail(t)
 	}
 
-	if MaxInt32 != 2147483647 {
+	if fmt.Sprintf("%v", MaxInt32) != "2147483647" {
 		fail(t)
 	}
 
-	if MinInt32 != -2147483648 {
+	if fmt.Sprintf("%v", MinInt32) != "-2147483648" {
 		fail(t)
 	}
 
-	if MaxInt40 != 549755813887 {
+	if fmt.Sprintf("%v", MaxInt40) != "549755813887" {
 		fail(t)
 	}
 
-	if MinInt40 != -549755813888 {
+	if fmt.Sprintf("%v", MinInt40) != "-549755813888" {
 		fail(t)
 	}
 
-	if MaxInt48 != 140737488355327 {
+	if fmt.Sprintf("%v", MaxInt48) != "140737488355327" {
 		fail(t)
 	}
 
-	if MinInt48 != -140737488355328 {
+	if fmt.Sprintf("%v", MinInt48) != "-140737488355328" {
 		fail(t)
 	}
 
-	if MaxInt56 != 36028797018963967 {
+	if fmt.Sprintf("%v", MaxInt56) != "36028797018963967" {
 		fail(t)
 	}
 
-	if MinInt56 != -36028797018963968 {
+	if fmt.Sprintf("%v", MinInt56) != "-36028797018963968" {
 		fail(t)
 	}
 
-	if MaxInt64 != 9223372036854775807 {
+	if fmt.Sprintf("%v", MaxInt64) != "9223372036854775807" {
 		fail(t)
 	}
 
-	if MinInt64 != -9223372036854775808 {
+	if fmt.Sprintf("%v", MinInt64) != "-9223372036854775808" {
 		fail(t)
 	}
 
-	if MaxUint8 != 255 {
+	if fmt.Sprintf("%v", MaxUint8) != "255" {
 		fail(t)
 	}
 
-	if MaxUint16 != 65535 {
+	if fmt.Sprintf("%v", MaxUint16) != "65535" {
 		fail(t)
 	}
 
-	if MaxUint24 != 16777215 {
+	if fmt.Sprintf("%v", MaxUint24) != "16777215" {
 		fail(t)
 	}
 
-	if MaxUint32 != 4294967295 {
+	if fmt.Sprintf("%v", MaxUint32) != "4294967295" {
 		fail(t)
 	}
 
-	if MaxUint40 != 1099511627775 {
+	if fmt.Sprintf("%v", MaxUint40) != "1099511627775" {
 		fail(t)
 	}
 
-	if MaxUint48 != 281474976710655 {
+	if fmt.Sprintf("%v", MaxUint48) != "281474976710655" {
 		fail(t)
 	}
 
-	if MaxUint56 != 72057594037927935 {
+	if fmt.Sprintf("%v", MaxUint56) != "72057594037927935" {
 		fail(t)
 	}
 
-	if MaxUint64 != 18446744073709551615 {
+	if fmt.Sprintf("%v", MaxUint64) != "18446744073709551615" {
 		fail(t)
 	}
 
 	a := MinInt8
-	if int32(a) != -128 {
+	if fmt.Sprintf("%v", int32(a)) != "-128" {
 		fail(t)
 	}
 }
